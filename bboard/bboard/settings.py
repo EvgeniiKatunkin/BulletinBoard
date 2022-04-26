ce58d90b5edb12f8e9f6952f401099fdc5cd8f69
EvgeniiKatunkin <evgeniikatunkin@gmail.com>
2022-04-26 17:00:03 +0500

    The app 'main' is added. It will be the main application of the site. The verbose name (Bulletin Board) for apps.py in main folder is added. Bootstrap4 library is added to the project and it's core is added in settings.py into INSTALLED_APPS ('bootstrap4'). The static stylesheet 'style.css' is added in 'main/static/main' folder. It's empty for now. The 'basic.html' file is created in 'main/templates/layout'. The code inside is ready to image the basics of the website: the menu, navigation and external libraries are added.

--- bboard/bboard/settings.py
+++ bboard/bboard/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'main',
+    'bootstrap4',
 ]
 
 MIDDLEWARE = [
@@ -76,7 +78,7 @@ WSGI_APPLICATION = 'bboard.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': BASE_DIR / 'bboard.data',
     }
 }
 
